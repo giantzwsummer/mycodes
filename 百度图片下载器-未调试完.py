803e0dbc03cf2041c12e5a61e74a65f34a91669e
giantzwsummer <giantzwsumer@gmail.com>
2016-08-04 22:09:48 +0800

    Add files via upload

--- 百度图片下载器-未调试完.py
+++ 百度图片下载器-未调试完.py
@@ -6,27 +6,32 @@ import time
 
 
 #'定义寻找页面里所有待爬取img地址并下载，防错机制'
-def get_imgurl(url,x,name):
+def get_imgurl(url):
+    time.sleep(5)
+    global x
+    global name
     try:
         headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'}
         r=requests.get(url, headers=headers)
         soup = BeautifulSoup(r.text,'html.parser',from_encoding='utf-8')
-        links =soup.find_all('img',src=re.compile(r"\.jpg"))
+        links =soup.find('ul',class_='imglist').find_all('a',class_='imglink')
+        
         for link in links:
-            src=link['src']
-            print(src)
-            x=down_load(src,x,name)
-            time.sleep(1)
+            href=link['href']
+            print(href)
+            down_load(href)
+           
         
         
     except:
-        print('fail to down %s第%x张照片:'%(name,x))
-    return x 
+        print('fail to deal with the url %s'%url)
+    
+    
     
 
 #'定义寻找所有待爬取url函数，防错机制'
 def get_url(url):
-    global undo                   
+    global count                   
     
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'}
     r=requests.get(url, headers=headers)
@@ -45,9 +50,9 @@ def get_url(url):
             if url_find not in undo:
                
                 undo.add(url_find)
-                time.sleep(1)
+               
                 get_url(url_find)
-        return undo 
+       
         
             
                    
@@ -57,7 +62,10 @@ def get_url(url):
             
         
 #‘定义图片下载函数’
-def down_load(src,x,name):
+def down_load(src):
+    time.sleep(5)
+    global x
+    global name
     print('正在下载%s第%d 张'%(name,x))
     
     try:
@@ -76,41 +84,40 @@ def down_load(src,x,name):
         
         
       
-    return x
+    
 
-def where_store():
-    #‘准备保存图片的文件夹'
-    location=input('where to store:')#'获得目录名'
-    
-    
-    location='e:\\lyx\\python download\\picture'+location#'自己的下载目录，可以根据需要修改'
-    if not os.path.exists(location):#'如果不存在，则创建并切换到这个目录'
-        os.makedirs(location) 
-        os.chdir(location)
-    else:  #'如果存在文件目录，覆盖输入1，放弃并结束程序输入0'
-        print('the file is already existed')
-        value=int(input('print "1" to go on,or "0"to exit '))
-        if value:
-            os.chdir(location)
-            print("OK")
-        elif not value:
-            print('Bye-ye')
-            os._exit(0)
+##def where_store():
+##    #‘准备保存图片的文件夹'
+##    location=input('where to store:')#'获得目录名'
+##    
+##    
+##    location='e:\\lyx\\python download\\picture'+location#'自己的下载目录，可以根据需要修改'
+##    if not os.path.exists(location):#'如果不存在，则创建并切换到这个目录'
+##        os.makedirs(location) 
+##        os.chdir(location)
+##    else:  #'如果存在文件目录，覆盖输入1，放弃并结束程序输入0'
+##        print('the file is already existed')
+##        value=int(input('print "1" to go on,or "0"to exit '))
+##        if value:
+##            os.chdir(location)
+##            print("OK")
+##        elif not value:
+##            print('Bye-ye')
+##            os._exit(0)
             
 def main():
-    where_store()
-    name=input('img name：')
     
+##    where_store()
     
     
     root_url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word='+name+'&ct=201326592&v=flip'
-    find_urls=get_url(root_url)
-    print('all found urls:')
-    x=0
+    get_url(root_url)
+    
+    
     for each in undo:
+        get_imgurl(each)
         
-        print(each)
-        x=get_imgurl(each,x,name)
+        
     print('All done') 
     print('the numbers of pictures downloaded is %d:'%x) 
         
@@ -118,10 +125,13 @@ def main():
                    
                         
 if  __name__ == '__main__':
+    global undo
+    global name
+    global x
     undo=set()
-    
+    name=input('img name：')
+    x=0
     main()
-    
 
 
 
